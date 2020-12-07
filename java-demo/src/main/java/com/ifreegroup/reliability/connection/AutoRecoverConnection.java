56ee0f357fd919cd2b422907b62ee6200b593981
liqiit <liqiit@163.com>
2020-12-07 16:40:32 +0800

    自动恢复连接

--- java-demo/src/main/java/com/ifreegroup/reliability/connection/AutoRecoverConnection.java
+++ java-demo/src/main/java/com/ifreegroup/reliability/connection/AutoRecoverConnection.java
@@ -23,7 +23,7 @@ public class AutoRecoverConnection {
     private static final String RABBITMQ_USERNAME = "guest";
     private static final String RABBITMQ_PASSWORD = "guest";
     private static final String RABBITMQ_VIRTUALHOST = "/";
-    private static final String RABBITMQ_HOSTNAME = "localhost";
+    private static final String RABBITMQ_HOSTNAME = "8.210.252.134";
     private static final int RABBITMQ_PORT = 5672;
 
     public static void main(String[] args) throws IOException, TimeoutException {
@@ -32,24 +32,21 @@ public class AutoRecoverConnection {
         factory.setUsername(RABBITMQ_USERNAME);
         factory.setPassword(RABBITMQ_PASSWORD);
         factory.setVirtualHost(RABBITMQ_VIRTUALHOST);
-        factory.setHost(RABBITMQ_HOSTNAME);
+        //factory.setHost(RABBITMQ_HOSTNAME);
         factory.setPort(RABBITMQ_PORT);
         //连接自动恢复配置项, 4.0.0之后版本默认启动
         factory.setAutomaticRecoveryEnabled(true);
         //如果第一次恢复连接失败，后续每隔10秒尝试恢复一次
         factory.setNetworkRecoveryInterval(10000);
-        //进行自动尝试连接其他服务器
-        Address[] addresses = {new Address("127.0.0.1"), new Address("127.0.0.1")};
-        factory.newConnection(addresses);
         //如果自动重连不能成功，需要手动捕获异常增加重连逻辑
-        Connection conn = null;
-        Channel channel=null;
+        Connection conn;
+        Channel channel;
         try {
             conn = factory.newConnection();
             //设置连接的唯一标识，名称将被忽略
             conn.setId("autoRecoverConnection");
             channel=conn.openChannel().get();
-
+            System.out.println(channel.getConnection());
         } catch (java.net.ConnectException e) {
             try {
                 Thread.sleep(5000);
@@ -60,13 +57,13 @@ public class AutoRecoverConnection {
             conn = factory.newConnection();
 
         }finally {
-            if(channel!=null){
-                //可以不显示调用，连接关闭会自动关闭通道
-                channel.close();
-            }
-            if(conn!=null){
-                conn.close();
-            }
+//            if(channel!=null){
+//                //可以不显示调用，连接关闭会自动关闭通道
+//                channel.close();
+//            }
+//            if(conn!=null){
+//                conn.close();
+//            }
 
         }
         //增加连接恢复监听器
