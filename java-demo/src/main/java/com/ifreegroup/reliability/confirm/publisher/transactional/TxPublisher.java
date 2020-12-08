286bfd841be9414b9fd94a3cd6fe3146ef053d77
liqiit <liqiit@163.com>
2020-12-08 11:32:22 +0800

    增加事务消息示例

---
+++ java-demo/src/main/java/com/ifreegroup/reliability/confirm/publisher/transactional/TxPublisher.java
@@ -0,0 +1,74 @@
+package com.ifreegroup.reliability.confirm.publisher.transactional;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Title: TxPublisher
+ * Description:事务channel不能进入确认模式
+ * 如果broker在从内存将消息写入磁盘的过程中宕机，有可能发生丢失消息的风险
+ * 通过事务保证消息的可靠性，AMQP 0.9.1只有此一种方式，重量级，
+ * 会大大降低吞吐量（官方数据会降低250倍）
+ * Company: iFree Group
+ *
+ * @author liqi
+ * @date 2020/12/8
+ */
+public class TxPublisher {
+    private static final String RABBITMQ_USERNAME = "guest";
+    private static final String RABBITMQ_PASSWORD = "guest";
+    private static final String RABBITMQ_VIRTUALHOST = "/";
+    private static final String RABBITMQ_HOSTNAME = "localhost";
+    private static final int RABBITMQ_PORT = 5672;
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        //用户guest默认只能连接本地服务器，如果需要连接远程服务需要
+        factory.setUsername(RABBITMQ_USERNAME);
+        factory.setPassword(RABBITMQ_PASSWORD);
+        factory.setVirtualHost(RABBITMQ_VIRTUALHOST);
+        factory.setHost(RABBITMQ_HOSTNAME);
+        factory.setPort(RABBITMQ_PORT);
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.openChannel().get()) {
+            AMQP.Tx.SelectOk selectOk = channel.txSelect();
+            System.out.println("selectOK:" + selectOk);
+            channel.exchangeDeclare("txMsgExchange", BuiltinExchangeType.DIRECT, true);
+            for (int i = 0; i < 10; i++) {
+                String message = "hello txMessage " + i;
+                //设置属性，包括消息ID，
+                AMQP.BasicProperties props = new AMQP.BasicProperties(
+                        "text",
+                        "utf-8",
+                        new HashMap<>(),
+                        1,
+                        1,
+                        "correlationId",
+                        "replyTo",
+                        "100",
+                        UUID.randomUUID().toString(),
+                        new Date(),
+                        "type",
+                        "guest",
+                        "appId",
+                        "clusterId"
+                        );
+                //构建器设置属性值不生效
+//                AMQP.BasicProperties props = new AMQP.BasicProperties();
+//                props.builder().messageId(UUID.randomUUID().toString()).build();
+                channel.basicPublish("txMsgExchange", "tx", props, message.getBytes("UTF-8"));
+            }
+
+            channel.txCommit();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java-demo/src/main/java/com/ifreegroup/reliability/confirm/publisher/transactional/TxReceiver.java
@@ -0,0 +1,61 @@
+package com.ifreegroup.reliability.confirm.publisher.transactional;
+
+import com.rabbitmq.client.*;
+
+/**
+ * Title: TxReceiver
+ * Description: 事务消息接收者
+ * Company: iFree Group
+ *
+ * @author liqi
+ * @date 2020/12/3
+ */
+public class TxReceiver {
+    private static final String RABBITMQ_USERNAME = "guest";
+    private static final String RABBITMQ_PASSWORD = "guest";
+    private static final String RABBITMQ_VIRTUALHOST = "/";
+    private static final String RABBITMQ_HOSTNAME = "localhost";
+    private static final int RABBITMQ_PORT = 5672;
+    private final static String QUEUE_NAME = "txMsgQueue";
+
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(RABBITMQ_HOSTNAME);
+        connectionFactory.setUsername(RABBITMQ_USERNAME);
+        connectionFactory.setPassword(RABBITMQ_PASSWORD);
+        connectionFactory.setVirtualHost(RABBITMQ_VIRTUALHOST);
+        connectionFactory.setPort(RABBITMQ_PORT);
+        try {
+            Connection connection = connectionFactory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.exchangeDeclare("txMsgExchange", BuiltinExchangeType.DIRECT, true);
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null).getQueue();
+
+            channel.queueBind(QUEUE_NAME, "txMsgExchange", "tx");
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+            channel.basicQos(1);
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                AMQP.BasicProperties properties = delivery.getProperties();
+                String messageID = properties.getMessageId();
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received TxMessage ID:'" + messageID + ",content:" + message + "'");
+                try {
+                    doWork();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("TxMessage '" + message + "' has done");
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                }
+            };
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void doWork() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+}
